301b8c259c3e5bc6e2603925d60d7ca489bbeb80
jlofwall <jlofwall@gmail.com>
2025-01-12 22:55:45 +0000

    Completed get_URL function in webget.cc

--- apps/webget.cc
+++ apps/webget.cc
@@ -10,7 +10,21 @@ using namespace std;
 void get_URL( const string& host, const string& path )
 {
   cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+  //cerr << "Warning: get_URL() has not been implemented yet.\n";
+  TCPSocket socket;
+  const Address address = Address(host, "http");
+  socket.connect(address);
+
+  const string request = "GET " + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n";
+
+  socket.write(request);
+
+  while (!socket.eof()) {
+    string buf; 
+    socket.read(buf);
+    cout << buf;
+  }
+  socket.close();
 }
 
 int main( int argc, char* argv[] )
